34e69420842d0f79a8a270c2930b356ba18e952a
Ming-der Wang <ming@log4analytics.com>
2020-06-20 12:23:00 +0800

    to use local raiden rpc endpoint

--- axiosInstance.js
+++ axiosInstance.js
@@ -5,7 +5,7 @@ const axios = require('axios')
 
 // default BASE_URL is "http:localhost:5001" where localhost is the host running your raiden node.
 
-const BASE_URL = "https://43cd29c1.ngrok.io"
+const BASE_URL = "http://localhost:5001"
 const instance = axios.create({
   baseURL: BASE_URL+'/api/v1/'
 })
